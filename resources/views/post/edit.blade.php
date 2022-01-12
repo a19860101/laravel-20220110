e437b3dc2e0033c4b921342ccf006386b4bea428
Zac <a19860101@gmail.com>
2022-01-12 21:46:04 +0800

    20220112

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,36 +1,27 @@
-!<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-8">
-                <h2>編輯文章</h2>
-            </div>
-            <div class="col-8">
-                <form action="{{route('post.update',['post'=>$post->id])}}" method="post">
-                    @csrf
-                    @method('put')
-                    <div class="mb-3">
-                        <label for="">標題</label>
-                        <input type="text" name="title" class="form-control" value="{{$post->title}}">
-                    </div>
-                    <div class="mb-3">
-                        <label for="">文章內容</label>
-                        <textarea name="content" id="content" cols="30" rows="10" class="form-control">{{$post->content}}</textarea>
-                    </div>
-                    <input type="submit" value="儲存" class="btn btn-primary">
-                    <input type="button" value="取消" class="btn btn-danger" onclick="history.back()">
-                </form>
-            </div>
+@extends('template.master')
+
+@section('main')
+<div class="container">
+    <div class="row">
+        <div class="col-8">
+            <h2>編輯文章</h2>
+        </div>
+        <div class="col-8">
+            <form action="{{route('post.update',['post'=>$post->id])}}" method="post">
+                @csrf
+                @method('put')
+                <div class="mb-3">
+                    <label for="">標題</label>
+                    <input type="text" name="title" class="form-control" value="{{$post->title}}">
+                </div>
+                <div class="mb-3">
+                    <label for="">文章內容</label>
+                    <textarea name="content" id="content" cols="30" rows="10" class="form-control">{{$post->content}}</textarea>
+                </div>
+                <input type="submit" value="儲存" class="btn btn-primary">
+                <input type="button" value="取消" class="btn btn-danger" onclick="history.back()">
+            </form>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
+</div>
+@endsection
