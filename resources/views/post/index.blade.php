f8ba9eaa5f21412e4497d088d7c27429a6c702e9
Zac <a19860101@gmail.com>
2022-01-17 19:28:40 +0800

    20220117

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -7,15 +7,15 @@
         <div class="col-8">
             <h3>{{$post->title}}</h3>
             <div>
-                {{$post->content}}
+                {!!Str::limit($post->content,200)!!}
             </div>
+            <a href="{{route('post.show',['post' => $post->id])}}" class="btn btn-primary">繼續閱讀</a>
             <div>
                 建立時間{{$post->created_at}}
             </div>
             <div>
                 最後更新時間{{$post->updated_at}}
             </div>
-            <a href="{{route('post.show',['post' => $post->id])}}" class="btn btn-primary">繼續閱讀</a>
             <hr>
         </div>
         @endforeach
