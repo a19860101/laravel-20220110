8b851df000677020368da24acf84c69c613fa2a6
Zac <a19860101@gmail.com>
2022-01-24 22:03:22 +0800

    20220124

--- app/Post.php
+++ app/Post.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
     //
+    use SoftDeletes;
     protected $fillable = ['title','content','cover','category_id','user_id'];
 
     public function category(){
