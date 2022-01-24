ee20430ffe283eca590d51854c4de23eaec222f3
Zac <a19860101@gmail.com>
2022-01-24 20:26:08 +0800

    20220124

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -8,6 +8,12 @@
         <div class="col-8">
             <h3>{{$post->title}}</h3>
             <div>
+                分類:{{$post->category->title}}
+            </div>
+            <div>
+                作者:{{$post->user->name}}
+            </div>
+            <div>
                 {!!$post->content!!}
             </div>
             <div>
@@ -17,12 +23,14 @@
                 最後更新時間{{$post->updated_at}}
             </div>
             <hr>
+            @if(Auth::id() == $post->user_id)
             <a href="{{route('post.edit',['post' => $post->id])}}" class="btn btn-primary">編輯文章</a>
             <form action="{{route('post.destroy',['post' => $post->id])}}" method="post" class="d-inline-block">
                 @csrf
                 @method('delete')
                 <input type="submit" class="btn btn-danger" value="刪除" onclick="return confirm('確認刪除？')">
             </form>
+            @endif
             <a href="{{route('post.index')}}" class="btn btn-success">文章列表</a>
         </div>
 
