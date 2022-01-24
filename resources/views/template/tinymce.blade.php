a1e2a2da777076ae0f6ce3d659ba40ffb84730b0
Zac <a19860101@gmail.com>
2022-01-24 21:21:05 +0800

    20220124

--- resources/views/template/tinymce.blade.php
+++ resources/views/template/tinymce.blade.php
@@ -5,7 +5,7 @@
         height:500,
         language: 'zh_TW',
         plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
-        toolbar: 'removeformat | undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+        toolbar: 'removeformat | insertfile image | undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor | pagebreak | charmap emoticons | fullscreen  preview save print | media template link anchor codesample | ltr rtl',
 
     });
   </script>
