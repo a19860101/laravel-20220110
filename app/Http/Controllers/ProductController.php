2186da7f9d073de4de67d7bf0dcfacf4b91f921e
Zac <a19860101@gmail.com>
2022-02-07 20:19:51 +0800

    20220207

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
     public function index()
     {
         //
-        $products = Product::get();
+        $products = Product::orderBy('id','DESC')->get();
         return view('product.index',compact('products'));
     }
 
