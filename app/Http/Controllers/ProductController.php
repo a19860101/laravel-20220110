e84cec0d68e017215233316ebe67c45ae334e6aa
Zac <a19860101@gmail.com>
2022-02-07 20:19:18 +0800

    20220207

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,8 @@ class ProductController extends Controller
     public function index()
     {
         //
-        return view('product.index');
+        $products = Product::get();
+        return view('product.index',compact('products'));
     }
 
     /**
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -11,8 +11,26 @@
                 <tr>
                     <th>#</th>
                     <th>商品名稱</th>
-                    <th>商品價格</th>
+                    <th>售價</th>
+                    <th>特價</th>
+                    <th>動作</th>
                 </tr>
+                @foreach ($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->title}}</td>
+                    <td>{{$product->price}}</td>
+                    <td>{{$product->sale}}</td>
+                    <td>
+                        <a href="#" class="btn btn-success">編輯商品資訊</a>
+                        <form action="" method="post" class="d-inline-block">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" class="btn btn-danger" value="刪除商品">
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
             </table>
         </div>
     </div>
