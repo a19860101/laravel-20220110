bd381073b2b6c0ec8d650684ef4101d86a185f4a
Zac <a19860101@gmail.com>
2022-02-09 21:32:01 +0800

    20220209

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Cart;
+use Auth;
 class CartController extends Controller
 {
     //
     public function addToCart(Request $request){
-        return $request;
+        $cart = new Cart;
+        $cart->product_id = $request->product_id;
+        $cart->user_id = Auth::id();
+
+        $cart->save();
+        return redirect()->back();
     }
 }
